1cdf4f3ffc7a382026f2fb0171f11cd2c2bd0f62
Israel José <israeljose@MBP-de-Israel.lan>
2020-11-27 23:46:02 -0300

    adicionando data_scaler2

--- prep_scaler.py
+++ prep_scaler.py
@@ -2,7 +2,7 @@ import pickle
 
 class prep_scaler( object):
 	def __int__(self):
-		self.prep = pickle.load(open('/Users/israeljose/OneDrive/Desafio_Comunidade_Ciencia_de_Dados_Suzano/modelos/prep_scaler.pkl', 'rb'))
+		self.prep = pickle.load(open('model/prep_scaler.pkl', 'rb'))
 
 	def data_preparacao(self, array):
 		array = self.prep.transform(array)
