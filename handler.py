989af445df71940f5f7e2eb4d42648de8ecd1242
Israel José <israeljose@MBP-de-Israel.lan>
2020-12-07 22:04:55 -0300

    atualizacao da pagina para input range

--- handler.py
+++ handler.py
@@ -1,18 +1,20 @@
 import pandas as pd
 from flask import Flask, request, render_template, url_for, redirect
+from flask_bootstrap import Bootstrap 
 import pickle
 import numpy as np
 import os
 import lightgbm as lgb
 
-from prep_scaler import prep_scaler
 
 ## Carregar  o modeelo na memoria para toda vez que inciar a API o modelo carregar, antes da solcitacao
 # carregar o modelo
-model_cl = pickle.load( open('model/modelo_Machine_Learning_PaperCO_CL.pkl', 'rb'))
-model_re = pickle.load( open('model/modelo_Machine_Learning_PaperCO_RE.pkl', 'rb'))
+model_cl = pickle.load( open('model/modelo_Machine_Learning_PaperCO_CL_8.pkl', 'rb'))
+model_re = pickle.load( open('model/modelo_Machine_Learning_PaperCO_RE_10.pkl', 'rb'))
+
 # instanciar flask
 app = Flask(__name__)
+Bootstrap(app)
 
 # endpoint
 @app.route('/')
@@ -24,37 +26,49 @@ def predict():
 
 #--------------------------------------------
 # Coleta de dados da pagina
+    #---- tudo em uma lista (1,4), poré apenas uma feature.
     lista = [float(x) for x in request.form.values()]
+    print("lista handler",lista)
+    print("lista handler",type(lista))
+    print("lista shape",np.array(lista).shape)
 
-    #----------SCALER------------------------
-    # transpose da lista
     lista1 = np.array(lista)
     lista1 = lista1.reshape(-1,1)
+    print("lista1 size: ",lista1.size)
+    #print('lista1 reshape',lista1.shape)
+    #----------SCALER------------------------
+    #---Para esse método, o shape precisa ser: lista1 reshape (4, 1)-----#
+    #--ou seja os dados devem estar em linhas....
 
-    range_min_max = [[1382.25, 46.85, 518.69, 8.3249],[1441.49, 48.53, 523.38, 8.5848]]
+    range_min_max = [[641.2100,1382.2500,549.8500,9021.7300,46.8500,518.6900,8099.9400,8.3249,38.1400,22.8942],[644.5300,1441.4900,556.0600,9244.5900,48.5300,523.3800,8293.7200,8.5848,39.4300,23.6184]]
     x_new = []
     for i in range(lista1.size):
-        x_new.append(( lista1[i] - range_min_max[0][i]) / (range_min_max[1][i] - range_min_max[0][i]))
-
+        x_new.append((lista1[i] - range_min_max[0][i]) / (range_min_max[1][i] - range_min_max[0][i]))
+    #print(x_new)
 
     df = pd.DataFrame(x_new)
+    indices_cl = [0,1,2,4,5,7,8,9]
+    indices_re = [0,1,2,3,4,5,6,7,8,9]
+    #print('df: ',df)
+    #print('df.T: ',df.T)
+    df_cl = df.iloc[indices_cl,:]
+    df_re = df.iloc[indices_re,:]
 #---------------------------------------------
-    pred_pagina_cl = model_cl.predict(df.T)
-    pred_pagina_re = np.int(model_re.predict(df.T))
+    pred_pagina_cl = model_cl.predict(df_cl.T)
+    pred_pagina_re = np.int(model_re.predict(df_re.T))
+    
+    #print('classificacao: {}'.format(pred_pagina_cl))
 #---------------------------------------------
-# Exibindo na pagina
+
     if pred_pagina_cl == 1:
-        return render_template('paperco.html',
-                               #pred='Máquina precisando de manutenção.\nDentro dos últimos 20 Ciclos.\nResultado da Predição: {}'.format(pred_pagina_cl),
-                               pred='Máquina precisando de manutenção.\nDentro dos últimos 20 Ciclos.',
-                               bhai='Próxima quebra será daqui {}'.format(pred_pagina_re) +' dias.')
+        pred='Ops! Máquina precisando de manutenção. Dentro dos últimos 20 Ciclos se espera uma parada.'
+        bhai='Próxima quebra será daqui {}'.format(pred_pagina_re) +' dias. Acione a equipe de manutenção.'
     else:
-        return render_template('paperco.html',
-                               pred='Máquina fora do risco de quebra.\n Ainda faltam {}'.format(pred_pagina_re),
-                               bhai="dias.")
+        pred='Muito bem, a máquina e suas variáveis estão cooperando para um bom funciomanento.' 
+        bhai='O Sistema de previsão diz que ainda restam {}'.format(pred_pagina_re) + ' dias.'
+    return render_template('paperco.html',predx = pred_pagina_cl, bhaix = pred_pagina_re, pred = pred , bhai = bhai ) 
+
 #--------------------------------------------
 if __name__ == '__main__':
-    # star flask
-    port = os.environ.get('PORT', 5000)
-    app.run(host  = '0.0.0.0', port = port)
+    app.run(host  = '0.0.0.0', port = '5000', debug=False)
 
---
+++ static/codepen2.js
@@ -0,0 +1,113 @@
+
+var tag2_in = document.getElementById("tag2_in");
+var tag2_out = document.getElementById("tag2_out");
+
+tag2_in.addEventListener("input", showSliderValue1, false);
+
+function showSliderValue1() {
+  tag2_out.innerHTML = tag2_in.value;
+  var bulletPosition = (tag2_in.value /tag2_in.max);
+  tag2_out.style.left = (bulletPosition * 578) + "px";
+}
+
+
+var tag4_in = document.getElementById("tag4_in");
+var tag4_out = document.getElementById("tag4_out");
+
+tag4_in.addEventListener("input", showSliderValue2, false);
+
+function showSliderValue2() {
+  tag4_out.innerHTML = tag4_in.value;
+  var bulletPosition = (tag4_in.value /tag4_in.max);
+  tag4_out.style.left = (bulletPosition * 578) + "px";
+}
+
+var tag7_in = document.getElementById("tag7_in");
+var tag7_out = document.getElementById("tag7_out");
+
+tag7_in.addEventListener("input", showSliderValue3, false);
+
+function showSliderValue3() {
+  tag7_out.innerHTML = tag7_in.value;
+  var bulletPosition = (tag7_in.value /tag7_in.max);
+  tag7_out.style.left = (bulletPosition * 578) + "px";
+}
+
+var tag9_in = document.getElementById("tag9_in");
+var tag9_out = document.getElementById("tag9_out");
+
+tag9_in.addEventListener("input", showSliderValue4, false);
+
+function showSliderValue4() {
+  tag9_out.innerHTML = tag9_in.value;
+  var bulletPosition = (tag9_in.value /tag9_in.max);
+  tag9_out.style.left = (bulletPosition * 578) + "px";
+}
+
+var tag11_in = document.getElementById("tag11_in");
+var tag11_out = document.getElementById("tag11_out");
+
+tag11_in.addEventListener("input", showSliderValue5, false);
+
+function showSliderValue5() {
+  tag11_out.innerHTML = tag11_in.value;
+  var bulletPosition = (tag11_in.value /tag11_in.max);
+  tag11_out.style.left = (bulletPosition * 578) + "px";
+}
+
+var tag12_in = document.getElementById("tag12_in");
+var tag12_out = document.getElementById("tag12_out");
+
+tag12_in.addEventListener("input", showSliderValue6, false);
+
+function showSliderValue6() {
+  tag12_out.innerHTML = tag12_in.value;
+  var bulletPosition = (tag12_in.value /tag12_in.max);
+  tag12_out.style.left = (bulletPosition * 578) + "px";
+}
+
+var tag14_in = document.getElementById("tag14_in");
+var tag14_out = document.getElementById("tag14_out");
+
+tag14_in.addEventListener("input", showSliderValue7, false);
+
+function showSliderValue7() {
+  tag14_out.innerHTML = tag14_in.value;
+  var bulletPosition = (tag14_in.value /tag14_in.max);
+  tag14_out.style.left = (bulletPosition * 578) + "px";
+}
+
+var tag15_in = document.getElementById("tag15_in");
+var tag15_out = document.getElementById("tag15_out");
+
+tag15_in.addEventListener("input", showSliderValue8, false);
+
+function showSliderValue8() {
+  tag15_out.innerHTML = tag15_in.value;
+  var bulletPosition = (tag15_in.value /tag15_in.max);
+  tag15_out.style.left = (bulletPosition * 578) + "px";
+}
+
+
+var tag20_in = document.getElementById("tag20_in");
+var tag20_out = document.getElementById("tag20_out");
+
+tag20_in.addEventListener("input", showSliderValue9, false);
+
+function showSliderValue9() {
+  tag20_out.innerHTML = tag20_in.value;
+  var bulletPosition = (tag20_in.value /tag20_in.max);
+  tag20_out.style.left = (bulletPosition * 578) + "px";
+}
+
+var tag21_in = document.getElementById("tag21_in");
+var tag21_out = document.getElementById("tag21_out");
+
+tag21_in.addEventListener("input", showSliderValue10, false);
+
+function showSliderValue10() {
+  tag21_out.innerHTML = tag21_in.value;
+  var bulletPosition = (tag21_in.value /tag21_in.max);
+  tag21_out.style.left = (bulletPosition * 578) + "px";
+}
+
